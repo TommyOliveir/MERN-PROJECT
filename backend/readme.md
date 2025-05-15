62c9657fce97a727685f842f0d35c1a2acc19d81
TommyOliveir <90339122+TommyOliveir@users.noreply.github.com>
2025-05-15 21:52:33 +0200

    eslint

---
+++ backend/readme.md
@@ -0,0 +1,17 @@
+install all packages
+
+1. node modules
+2. express
+3. mogoose
+4. dotenv
+5. nodemon
+
+# We use module import
+
+install express types for typscript
+
+# commands
+
+npx eslint --init
+npm init -y
+npx tsc --init
---
+++ backend/src/server.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Server is ready");
+});
+
+console.log(process.env.MONGO_URI);
+
+app.listen(5000, () => {
+  console.log("Server started at http://localhost:5000");
+});
+
+// m4rYW8n5B0se62rO
